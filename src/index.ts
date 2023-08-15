ff1a79c472796b742d2878aabdf302b2af1e1db3
Idooru <shere1765@gmail.com>
2023-08-15 20:46:55 +0900

    라우터 미들웨어 적용

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express, { Request, Response, json, urlencoded } from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import bookRouter from "./routes/book-router";
+
 const app: express.Application = express();
-const port: number = 3000;
+const port: number = 8080;
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use("/books", bookRouter);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}! \n`);
